84fc75fe560c03748703c36f473a33d4a8637ad6
CY Cheng <cy.cheng@eastech.com.hk>
2026-05-18 14:10:48 +0800

    fix: CORS policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://s1155160221.github.io/frontend-test",
+    "https://s1155160221.github.io",
     # add other frontend origins here if needed, e.g. Vite dev server:
     # "http://127.0.0.1:5173",
     # "http://localhost:5173",
